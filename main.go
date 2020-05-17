3c875d93a856117d7de0ae05738af53c6ea2daba
todoroki <s18.todoroki+git@gmail.com>
2020-05-17 22:48:20 +0900

    Add items by ghq list command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 
 	aw "github.com/deanishe/awgo"
@@ -21,9 +22,13 @@ func run() {
 	app := &cli.App{
 		Action: func(c *cli.Context) error {
 			query := getQuery(c.Args().First())
+			repos := getRepos(execGhq())
 			wf.NewItem("First result!").Valid(true)
 			wf.NewItem("Second result!").Valid(true)
 			wf.NewItem(query).Valid(true)
+			for _, repo := range repos {
+				wf.NewItem(repo).Valid(true)
+			}
 			wf.SendFeedback()
 			return nil
 		},
@@ -41,4 +46,18 @@ func main() {
 
 func getQuery(arg string) string {
 	return strings.Trim(arg, "\n")
+}
+
+func getRepos(bytes []byte) []string {
+	trim := strings.Trim(string(bytes), "\n")
+	return strings.Split(trim, "\n")
+}
+
+func execGhq() []byte {
+	command := os.Getenv("ghq")
+	out, err := exec.Command(command, "list", "-p").Output()
+	if err != nil {
+		return []byte{}
+	}
+	return out
 }
--- main_test.go
+++ main_test.go
@@ -12,3 +12,17 @@ func TestGetQuery(t *testing.T) {
 		t.Fatal(fmt.Sprintf("failed: getQuery() returns %s", result))
 	}
 }
+
+func TestGetRepos(t *testing.T) {
+	bytes := []byte{'a', '\n', 'b', '\n'}
+	result := getRepos(bytes)
+	if len(result) != 2 {
+		t.Fatal(fmt.Sprintf("failed: getRepos() returns %d contents", len(result)))
+	}
+	if result[0] != "a" {
+		t.Fatal(fmt.Sprintf("failed: getRepos().first returns %s", result[0]))
+	}
+	if result[1] != "b" {
+		t.Fatal(fmt.Sprintf("failed: getRepos().second returns %s", result[1]))
+	}
+}
