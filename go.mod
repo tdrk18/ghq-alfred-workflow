12abf618004888db0a4c0655803558c5ae681453
todoroki <s18.todoroki+git@gmail.com>
2020-05-17 21:18:55 +0900

    Implement sample workflow

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tdrk18/ghq-alfred-workflow
 
 go 1.14
+
+require github.com/deanishe/awgo v0.22.1
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main()  {
+import (
+	aw "github.com/deanishe/awgo"
+)
 
+var (
+	wf *aw.Workflow
+)
+
+func init() {
+	wf = aw.New()
+}
+
+func run() {
+	wf.NewItem("First result!").Valid(true)
+	wf.NewItem("Second result!").Valid(true)
+	wf.SendFeedback()
+}
+
+func main() {
+	wf.Run(run)
 }
