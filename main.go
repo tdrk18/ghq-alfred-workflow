dde49063b0a34c85d6a8dba5f64fbab16bfd198b
todoroki <s18.todoroki+git@gmail.com>
2020-05-24 16:08:06 +0900

    Move to repository.go

--- main.go
+++ main.go
@@ -55,43 +55,6 @@ func getQuery(arg string) string {
 	return strings.Trim(arg, "\n")
 }
 
-func getRepoName(repo string, excludeDomain bool) string {
-	repoPath := strings.Split(repo, "/")
-	length := len(repoPath)
-	var i int
-	if excludeDomain {
-		i = 2
-	} else {
-		i = 3
-	}
-	return strings.Join(repoPath[length-i:length], "/")
-}
-
-func getDomain(repo string) string {
-	repoPath := strings.Split(repo, "/")
-	length := len(repoPath)
-	return repoPath[length - 3]
-}
-
-func getRepoURL(repo string) string {
-	repoName := getRepoName(repo, false)
-	return fmt.Sprintf("https://%s", repoName)
-}
-
-func getIconName(repo string) string {
-	domain := getDomain(repo)
-	switch {
-	case strings.Contains(domain, "github"):
-		return "github.png"
-	case strings.Contains(domain, "bitbucket"):
-		return "bitbucket.png"
-	case strings.Contains(domain, "gitlab"):
-		return "gitlab.png"
-	default:
-		return "git.png"
-	}
-}
-
 func getIcon(repo string) *aw.Icon {
 	return &aw.Icon{Value: path.Join(fmt.Sprintf("resources/%s", getIconName(repo)))}
 }
